116b3232d7245ab9593d8dbc43a28817970d1c99
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-08-28 17:59:59 +0900

    APIView 사용

--- django_app/snippets/serializers.py
+++ django_app/snippets/serializers.py
@@ -1,9 +1,9 @@
-from django.forms import widgets
 from rest_framework import serializers
+
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer1(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
@@ -28,3 +28,16 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = (
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style'
+        )
--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),
-    url(r'^(?P<pk>\d+)/$', views.snippet_detail),
+    url(r'^$', views.SnippetList.as_view()),
+    url(r'^(?P<pk>\d+)/$', views.SnippetDetail.as_view()),
 ]
